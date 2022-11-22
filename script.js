7bbb5a9066a5890789deea05a6359f0b2ab5f725
Elliot Hipski <elhip03@gmail.com>
2022-11-22 13:54:50 -0500

    finished part of the toPostfix method

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const DISPLAY = document.getElementById("main-d");
 
+const OPERATORS = ['/', '*', '-', '+', '^']
+
 // Buttons
-document.getElementById("+").addEventListener("click", pick);
-document.getElementById("-").addEventListener("click", pick);
-document.getElementById("*").addEventListener("click", pick);
-document.getElementById("/").addEventListener("click", pick);
+document.getElementById("+").addEventListener("click", pickOperator);
+document.getElementById("-").addEventListener("click", pickOperator);
+document.getElementById("*").addEventListener("click", pickOperator);
+document.getElementById("/").addEventListener("click", pickOperator);
 
 /**
  * string for the equation
@@ -39,6 +41,18 @@ class Queue {
     isEmpty() {
         return this.size==0;
     }
+
+    toString() {
+        let s = "";
+
+        for (let i = this.head; i < this.content.length(); i++) {
+            s += this.content[i].toString()
+
+            if (i < this.content.length() - 21) {
+                s += ", ";
+            }
+        }
+    }
 }
 
 class Stack {
@@ -69,6 +83,8 @@ class Stack {
 let outputQueue = new Queue();
 let operatorStack = new Stack();
 
+let numberQueue = new Queue();
+
 let selected = new Queue();
 
 var pMap = new Map();
@@ -79,10 +95,14 @@ pMap.set('+', 1);
 pMap.set('-', 1);
 pMap.set('*', 2);
 pMap.set('/', 2);
+pMap.set('^', 3);
 
 
 // code: 
 
+/**
+ * creates a grid of numbers for the calculater as a User Intterface
+ */
 function createNumbers() {
     for (let i = 0; i <= 10; i++) {
 
@@ -106,6 +126,9 @@ createNumbers();
 
 /**
  * eventListener function, allows user to pick a number for selection queue
+ * 
+ * @param e
+ * event handler param
  */
 function pick(e) {
 
@@ -123,6 +146,62 @@ function pick(e) {
  */
 function setDisplay(d) {
     DISPLAY.innerHTML = d;
+}
+
+/**
+ * function to pick an operator
+ * 
+ * @param e
+ * event parameter
+ */
+function pickOperator(e) {
+    operatorStack.push(e.target.innerHTML);
+    numberQueue.enqueue(Number(selected.toString()));
+    selected = selected.clear();
+    equationString += e.target.innerHTML;
+
+}
+
+/**
+ * 
+ * 
+ * needed data: operatorStack, outputQueue, numberQueue, equationString
+ */
+function operate() {
+
+    let index1 = 0;
+    let index2 = 0;
+
+    let postfixString = "";
+
+    for (let i = 0; i < equationString.length(); i++) {
+
+        if (!operators.contains(equationString.charAt(i))) {
+
+            index2++;
+
+            postfixString += equationString.charAt(i);
+            
+        }
+
+        if (operators.contains(equationString.charAt(i))) {
+
+            let operator = equationString.charAt(i);
+
+
+
+            outputQueue.enqueue(Number(equationString().substring(index1, index2)));
+
+            if (pMap.get.(operatorStack.peek())) {
+                operatorStack.push(operator);
+            }
+
+            index2++;
+            index1 = index2;
+            
+
+        }
+    }
 }
 
 setDisplay(equationString);
